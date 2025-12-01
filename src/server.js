e19233196d8ae337074122697a2b0a721f76a1c5
arodoli <aro.contacto.isla@gmail.com>
2025-12-01 11:54:14 +0100

    Initial structure

---
+++ src/server.js
@@ -0,0 +1,31 @@
+// Importamos la librería Express
+const express = require('express');
+// Importar morgan para logging HTTP
+const morgan = require('morgan');
+
+// Creamos la aplicación de Express
+const app = express();
+
+// MIDDLEWARES Y CONFIGURACIONES
+app.use(morgan('combined')); // Registro de solicitudes HTTP
+
+// Definimos el puerto. Render nos lo dará en una variable de entorno,
+// si no, usamos el 3000 para local.
+const PORT = process.env.PORT || 3000;
+
+// Definimos una ruta "Home" (/)
+app.get('/', (req, res) => {
+  res.send('Bienvenido a TextMaster API. Servidor: ',);
+});
+
+/*app.get('/health', (req, res) => {
+  res.json({
+    status: 'OK',
+    timestamp: new Date(),
+    uptime: process.uptime() });
+});*/
+
+// Ponemos el servidor a "escuchar" en el puerto definido
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
